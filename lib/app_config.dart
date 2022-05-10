6f6f6820cf74e2498ccfa179e41a92dcc95137a5
mac-0009 <abrar.malekji@mindinventory.com>
2022-05-10 15:24:44 +0530

    model UI fix

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,14 +1,33 @@
-final appConfig=ProductionConfig();
+final appConfig=DevelopmentConfig();
 
 abstract class AppConfig{
   bool get loggerEnable;
+
+  String get email;
+  String get password;
 }
+
+
+
 class DevelopmentConfig extends AppConfig{
   @override
   bool get loggerEnable => true;
+
+  @override
+  String get email => 'test1@mailinator.com';
+
+  @override
+  String get password => 'password';
+
+
 }
 
 class ProductionConfig extends AppConfig {
   @override
   bool get loggerEnable => false;
+  @override
+  String get email => '';
+
+  @override
+  String get password => '';
 }
