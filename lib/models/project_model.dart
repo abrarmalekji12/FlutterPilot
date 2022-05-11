1ba11970e4b7275273f40b5a30ab3d11567d2a0c
mac-0009 <abrar.malekji@mindinventory.com>
2022-05-11 11:32:13 +0530

    fixed new project issue

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -35,8 +35,9 @@ class FlutterProject {
     final ui=UIScreen.mainUI();
     flutterProject.uiScreens.add(ui);
     final custom=StatelessComponent(name: 'MainPage')..root=CScaffold();
+
     flutterProject.customComponents.add(custom);
-    (ui.rootComponent as CMaterialApp).childMap['home']=custom.createInstance(null);
+    (ui.rootComponent as CMaterialApp).addOrUpdateChildWithKey('home', custom.createInstance(null));
 
     flutterProject.currentScreen = flutterProject.uiScreens.first;
     flutterProject.mainScreen = flutterProject.uiScreens.first;
